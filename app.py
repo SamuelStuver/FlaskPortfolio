33b74badfd7693d6d2434179cb526314297b46a2
Sam Stuver <samuelstuver@gmail.com>
2020-03-07 12:03:00 -0700

    add templates

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+
+posts = [
+    {
+        "title": "Post 1",
+        "author": "Sam Stuver",
+        "date_posted": "March 7, 2020",
+        "content": "First post content"
+    },
+    {
+        "title": "Post 2",
+        "author": "Sam Stuver",
+        "date_posted": "March 7, 2020",
+        "content": "Second post content"
+    }
+]
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title="About")
 
 
 if __name__ == '__main__':
